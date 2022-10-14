dbbdec3455a367923bac7cd4fcd3bcd4d0120c31
codewyx <66934040+codewyx@users.noreply.github.com>
2022-10-14 20:07:11 +0800

    Add files via upload

---
+++ js/major.js
@@ -0,0 +1,13 @@
+$(".Control-button").click(function(){
+    
+})
+$(document).ready(function(){
+  $("#flip1").click(function(){
+    $("#panel1").slideToggle("slow");
+  });
+});
+$(document).ready(function(){
+  $("#flip2").click(function(){
+    $("#panel2").slideToggle("slow");
+  });
+});
